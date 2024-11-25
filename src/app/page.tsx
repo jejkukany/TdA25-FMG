441643114f599ef83e32b5273f0fde7fd587a6c7
Daniel Young <young256daniel@gmail.com>
2024-11-25 19:25:25 +0100

    First try

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
+import { db } from "@/server/db";
+import { users } from "@/server/db/schema";
+
+export default async function Home() {
+  const data = await db.select().from(users).all();
+  console.log(data);
   return <div>Hello TdA</div>;
 }
