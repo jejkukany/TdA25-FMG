43e4ce684abe0bf1445fa6cd2378f6fe593be9a5
Daniel Young <young256daniel@gmail.com>
2024-12-02 21:36:34 +0100

    Quick test

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -4,6 +4,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const data = await db.select().from(users).all();
-  // console.log(data);
+  console.log(data);
   return NextResponse.json({ data: data }, { status: 200 });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,9 +23,11 @@ function handleError(error: unknown): string {
 }
 
 export default async function Home() {
-  const apiUrl = `${process.env.URL ?? "https://13682ac4.app.deploy.tourde.app"}/api/test`;
+  // const apiUrl = `${process.env.URL ?? "https://13682ac4.app.deploy.tourde.app"}/api/test`;
   try {
-    const testData = await fetchData(apiUrl);
+    const testData = await fetchData(
+      "https://13682ac4.app.deploy.tourde.app/api/test",
+    );
     return <div>Hello TdA {JSON.stringify(testData)}</div>;
   } catch (error) {
     const errorMessage = handleError(error);
