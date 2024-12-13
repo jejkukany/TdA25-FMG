390734ca48e8f5b05e009a92c624c696c35789bc
Daniel Young <young256daniel@gmail.com>
2024-12-13 08:55:35 +0100

    Error handling on route

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,7 +3,12 @@ import { users } from "@/server/db/schema";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const data = await db.select().from(users).all();
-  console.log(data);
-  return NextResponse.json({ data: data }, { status: 200 });
+  try {
+    const data = await db.select().from(users).all();
+    console.log(data);
+    return NextResponse.json({ data: data }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
 }
