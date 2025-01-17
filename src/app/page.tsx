35f0bd260d817d3ecf6195b9455b40d77138669a
Daniel Young <young256daniel@gmail.com>
2025-01-17 21:54:51 +0100

    Mobile optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,62 +8,58 @@ import Image from "next/image";
 import ThemeImage from "@/components/custom/ThemeImage";
 
 export default function Home() {
-  // Initial empty board or with only a few starting marks
   const initialBoard = [
-    ...Array(5).fill(Array(15).fill("")), // Top empty rows
-    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], // Starting state (few moves)
+    ...Array(5).fill(Array(15).fill("")),
     ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
     ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
     ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
     ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
-    ...Array(5).fill(Array(15).fill("")), // Bottom empty rows
+    ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+    ...Array(5).fill(Array(15).fill("")),
   ];
 
   const [board, setBoard] = useState(initialBoard);
-  const [moveIndex, setMoveIndex] = useState(0); // Index for the next move
+  const [moveIndex, setMoveIndex] = useState(0);
 
-  // Predefined game moves based on the configuration
   const predefinedMoves = [
-    { row: 7, col: 7, player: "X" }, // First move
-    { row: 7, col: 8, player: "O" }, // Counter move
-    { row: 6, col: 7, player: "X" }, // X starts forming a vertical line
-    { row: 6, col: 8, player: "O" }, // O blocks horizontally
-    { row: 8, col: 7, player: "X" }, // X continues vertical line
-    { row: 8, col: 8, player: "O" }, // O continues blocking
-    { row: 5, col: 7, player: "X" }, // X continues vertical line
-    { row: 9, col: 8, player: "O" }, // O moves in an empty spot
-    { row: 9, col: 7, player: "X" }, // X completes vertical line (wins!)
-    { row: 6, col: 6, player: "O" }, // O tries to create a different line
-    { row: 6, col: 9, player: "X" }, // X counters the move
-    { row: 7, col: 6, player: "O" }, // O tries building horizontally
-    { row: 7, col: 9, player: "X" }, // X secures another potential win
+    { row: 7, col: 7, player: "X" },
+    { row: 7, col: 8, player: "O" },
+    { row: 6, col: 7, player: "X" },
+    { row: 6, col: 8, player: "O" },
+    { row: 8, col: 7, player: "X" },
+    { row: 8, col: 8, player: "O" },
+    { row: 5, col: 7, player: "X" },
+    { row: 9, col: 8, player: "O" },
+    { row: 9, col: 7, player: "X" },
+    { row: 6, col: 6, player: "O" },
+    { row: 6, col: 9, player: "X" },
+    { row: 7, col: 6, player: "O" },
+    { row: 7, col: 9, player: "X" },
   ];
 
-  // Function to gradually add moves to the board
   const makeMove = () => {
     if (moveIndex >= predefinedMoves.length) return;
 
     const { row, col, player } = predefinedMoves[moveIndex];
     const newBoard = [...board];
-    newBoard[row][col] = player; // Place the player's mark on the board
+    newBoard[row][col] = player;
     setBoard(newBoard);
 
-    setMoveIndex(moveIndex + 1); // Move to the next move in the sequence
+    setMoveIndex(moveIndex + 1);
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
-      makeMove(); // Make a move every 1.5 seconds
-    }, 500); // 1000 milliseconds = 1
-    console.log(board);
+      makeMove();
+    }, 500);
 
-    return () => clearInterval(interval); // Cleanup interval on component unmount
+    return () => clearInterval(interval);
   }, [moveIndex]);
 
   return (
     <div className="min-h-screen bg-background flex flex-col items-center justify-center gap-16">
       {/* Main Content */}
-      <main className="container mx-auto p-4 md:p-6 flex flex-row gap-8 items-center justify-between w-full h-[90vh]">
+      <main className="container mx-auto p-4 md:p-6 flex flex-col-reverse md:flex-row gap-8 items-center md:justify-between w-full md:h-[90vh] text-center md:text-left">
         {/* Call to Action */}
         <motion.div
           initial={{ opacity: 0, x: -100 }}
@@ -75,13 +71,14 @@ export default function Home() {
             Think Different Academy
           </h1>
           <p className="text-muted-foreground">
-            Challenge yourself by playing Tic Tac Toe Online.
+            Challenge yourself by playing Tic Tac Toe Online. Improve your
+            strategy and take on various puzzles!
           </p>
 
-          <div className="space-y-4 flex flex-col">
+          <div className="space-y-4 flex flex-col items-center md:items-start">
             <Link href="/game" className="w-1/2">
               <Button
-                className="w-full h-10 md:h-12 text-xs sm:text-sm md:text-base font-medium px-2 sm:px-4"
+                className="w-full h-12 font-medium px-2 sm:px-4"
                 size="lg"
               >
                 Local Multiplayer
@@ -91,7 +88,7 @@ export default function Home() {
             <Link href="/games" className="w-1/2">
               <Button
                 variant="secondary"
-                className="w-full h-10 md:h-12 text-xs sm:text-sm md:text-base font-medium px-2 sm:px-4"
+                className="w-full h-12 font-medium px-2 sm:px-4"
                 size="lg"
               >
                 Puzzles
@@ -134,6 +131,22 @@ export default function Home() {
                     }}
                   >
                     {cell === "X" && (
+                      <motion.img
+                        src="/TDA/X_modre.svg"
+                        alt="X"
+                        className="w-3/4 h-3/4"
+                        initial={{ scale: 0, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        exit={{ scale: 0, opacity: 0 }}
+                        transition={{
+                          duration: 0.4,
+                          ease: "easeInOut",
+                        }}
+                      />
+                    )}
+                    {cell === "O" && (
+                      <motion.img
+                        src="/TDA/O_cervene.svg"
                         alt="O"
                         className="w-3/4 h-3/4"
                         initial={{ scale: 0, opacity: 0 }}
@@ -153,7 +166,7 @@ export default function Home() {
         </motion.div>
       </main>
 
-      {/* Sections */}
+      {/* Section 1 */}
       <motion.section
         initial={{ opacity: 0, y: 100 }}
         whileInView={{ opacity: 1, y: 0 }}
@@ -162,13 +175,16 @@ export default function Home() {
           duration: 0.8,
           ease: [0.17, 0.67, 0.83, 0.67],
         }}
-        className="container mx-auto p-4 md:p-6 flex flex-row-reverse gap-8 items-center justify-between w-full h-[75vh]"
+        className="container mx-auto p-4 md:p-6 flex flex-col-reverse md:flex-row-reverse  gap-8 items-center justify-between w-full h-[75vh]"
       >
         <div className="flex-1 flex flex-col gap-6 text-center">
           <h2 className="text-3xl md:text-4xl font-bold mb-1">
             Local Multiplayer
           </h2>
-          <p className="text-muted-foreground">Play local with your friends!</p>
+          <p className="text-muted-foreground">
+            Challenge your friends to a thrilling game of Tic Tac Toe! Show your
+            strategic moves and outsmart your opponent!
+          </p>
           <div className="flex justify-center">
             <Link href="/game" className="w-1/2">
               <Button className="w-full h-12 text-lg" size="lg">
@@ -183,10 +199,16 @@ export default function Home() {
           transition={{ duration: 0.6, ease: "backOut" }}
           className="flex-1 flex justify-center"
         >
-          <Image src="/Fighting_HomePage.svg" alt="Local Multiplayer" width={1000} height={1000} />
+          <Image
+            src="/Fighting_HomePage.svg"
+            alt="Local Multiplayer"
+            width={1000}
+            height={1000}
+          />
         </motion.div>
       </motion.section>
 
+      {/* Section 2 */}
       <motion.section
         initial={{ opacity: 0, y: 100 }}
         whileInView={{ opacity: 1, y: 0 }}
@@ -195,11 +217,14 @@ export default function Home() {
           duration: 0.8,
           ease: [0.17, 0.67, 0.83, 0.67],
         }}
-        className="container mx-auto p-4 md:p-6 flex flex-row gap-8 items-center justify-between w-full h-[75vh]"
+        className="container mx-auto p-4 md:p-6 flex flex-col-reverse md:flex-row gap-8 items-center justify-between w-full h-[75vh]"
       >
         <div className="flex-1 flex flex-col gap-6 text-center">
           <h2 className="text-3xl md:text-4xl font-bold mb-1">Puzzles</h2>
-          <p className="text-muted-foreground">Try to resolve our puzzles!</p>
+          <p className="text-muted-foreground">
+            Get ready for a variety of challenging puzzles! Test your
+            problem-solving skills and see how quickly you can solve them.
+          </p>
           <div className="flex justify-center">
             <Link href="/games" className="w-1/2">
               <Button className="w-full h-12 text-lg" size="lg">
