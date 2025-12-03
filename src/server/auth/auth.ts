a09af1bda3c7321360ebefa5ee25d5e658717132
Daniel Young <young256daniel@gmail.com>
2025-12-03 20:27:27 +0100

    Small fix for better auth

--- src/server/auth/auth.ts
+++ src/server/auth/auth.ts
@@ -1,52 +1,53 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db/index";
-import { admin, username, type BetterAuthPlugin } from "better-auth/plugins";
+import { admin, username } from "better-auth/plugins";
+import type { BetterAuthPlugin } from "better-auth";
 
 export const userStatsPlugin = () => {
-	return {
-		id: "user-stats-plugin",
-		schema: {
-			user: {
-				fields: {
-					uuid: {
-						type: "string",
-						unique: true,
-					},
-					elo: {
-						type: "number",
-						required: true,
-					},
-					wins: {
-						type: "number",
-						required: true,
-					},
-					draws: {
-						type: "number",
-						required: true,
-					},
-					losses: {
-						type: "number",
-						required: true,
-					},
-				},
-			},
-		},
-	} satisfies BetterAuthPlugin;
+  return {
+    id: "user-stats-plugin",
+    schema: {
+      user: {
+        fields: {
+          uuid: {
+            type: "string",
+            unique: true,
+          },
+          elo: {
+            type: "number",
+            required: true,
+          },
+          wins: {
+            type: "number",
+            required: true,
+          },
+          draws: {
+            type: "number",
+            required: true,
+          },
+          losses: {
+            type: "number",
+            required: true,
+          },
+        },
+      },
+    },
+  } satisfies BetterAuthPlugin;
 };
 
 export const auth = betterAuth({
-	database: drizzleAdapter(db, {
-		provider: "sqlite", // or "mysql", "sqlite"
-	}),
-	plugins: [userStatsPlugin(), username(), admin()],
-	emailAndPassword: {
-		enabled: true,
-	},
-	user: {
-		changeEmail: {
-			enabled: true,
-		},
-	},
-	trustedOrigins: ["https://13682ac4.app.deploy.tourde.app"],
+  database: drizzleAdapter(db, {
+    provider: "sqlite", // or "mysql", "sqlite"
+  }),
+  plugins: [userStatsPlugin(), username(), admin()],
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    changeEmail: {
+      enabled: true,
+    },
+  },
+  trustedOrigins: ["https://13682ac4.app.deploy.tourde.app"],
 });
